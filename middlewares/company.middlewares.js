8fdf396dac602d58c584d21e499659e0615ac4db
Alberto Gómez <albertogomrod5@gmail.com>
2023-03-10 13:34:22 +0100

    Correciones de roles en los middlewares

--- middlewares/company.middlewares.js
+++ middlewares/company.middlewares.js
@@ -2,6 +2,7 @@ const { expressjwt } = require("express-jwt");
 
 const isCompany = (req, res, next) => {
     if(req.payload.role !== "company") {
+        res.status(405).json({errorMessage: "No tienes este acceso permitido."})
         return null
     } else {
         next()
