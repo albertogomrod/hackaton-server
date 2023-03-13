d4d83fbde7a79f66a85ab9a586b138c9791e9d84
AaronBarcos <abarcoscaballero@gmail.com>
2023-03-13 10:28:01 +0100

    descomentado cloudinary

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,35 +1,35 @@
-// const router = require("express").Router();
+const router = require("express").Router();
 
-// const uploaderHackaton = require("../middlewares/cloudinary-hackaton.config.js");
-// const uploaderTutorial = require("../middlewares/cloudinary-tutorial.config.js");
-// const uploaderProfile = require("../middlewares/cloudinary-profile.config.js");
+const uploaderHackaton = require("../middlewares/cloudinary-hackaton.config.js");
+const uploaderTutorial = require("../middlewares/cloudinary-tutorial.config.js");
+const uploaderProfile = require("../middlewares/cloudinary-profile.config.js");
 
-// // POST "/api/upload"
-// router.post("/hackaton", uploaderHackaton.single("image"), (req, res, next) => {
-//   if (!req.file) {
-//     next("No file uploaded!");
-//     return;
-//   }
+// POST "/api/upload"
+router.post("/hackaton", uploaderHackaton.single("image"), (req, res, next) => {
+  if (!req.file) {
+    next("No file uploaded!");
+    return;
+  }
 
-//   res.json({ imageUrl: req.file.path });
-// });
+  res.json({ imageUrl: req.file.path });
+});
 
-// router.post("/tutorial", uploaderTutorial.single("image"), (req, res, next) => {
-//   if (!req.file) {
-//     next("No file uploaded!");
-//     return;
-//   }
+router.post("/tutorial", uploaderTutorial.single("image"), (req, res, next) => {
+  if (!req.file) {
+    next("No file uploaded!");
+    return;
+  }
 
-//   res.json({ imageUrl: req.file.path });
-// });
+  res.json({ imageUrl: req.file.path });
+});
 
-// router.post("/profile", uploaderProfile.single("image"), (req, res, next) => {
-//   if (!req.file) {
-//     next("No file uploaded!");
-//     return;
-//   }
+router.post("/profile", uploaderProfile.single("image"), (req, res, next) => {
+  if (!req.file) {
+    next("No file uploaded!");
+    return;
+  }
 
-//   res.json({ imageUrl: req.file.path });
-// });
+  res.json({ imageUrl: req.file.path });
+});
 
-// module.exports = router;
+module.exports = router;
