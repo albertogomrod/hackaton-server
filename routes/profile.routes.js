c7f8f6e024508e7948b7b6372fbefcce18616fd6
AaronBarcos <abarcoscaballero@gmail.com>
2023-03-25 13:24:01 +0100

    ruta cambiar contraseña terminada

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -8,7 +8,7 @@ const isCompany = require("../middlewares/company.middlewares");
 const isAdmin = require("../middlewares/admin.middlewares");
 // PROFILE
 
-// GET "/api/profile/:id" => Renderiza un perfil de usuario
+// GET "/api/profile" => Renderiza un perfil de usuario
 
 router.get("/", isAuthenticated, async (req, res, next) => {
   try {
@@ -19,21 +19,17 @@ router.get("/", isAuthenticated, async (req, res, next) => {
   }
 });
 
-// PATH "/api/profile/edit/:id" => Actualiza perfil de usuario
+// PATH "/api/profile/edit" => Actualiza perfil de usuario
 router.patch("/edit", isAuthenticated, async (req, res, next) => {
-  const { username, email, profilephoto, comunidadAutonoma, password, tech } =
+  const { username, email, profilephoto, comunidadAutonoma, tech } =
     req.body;
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
-
     await User.findByIdAndUpdate(req.payload._id, {
       username,
       email,
       profilephoto,
       comunidadAutonoma,
-      password: hashPassword,
-      tech,
+      tech
     });
     res.json("Usuario actualizado");
   } catch (error) {
@@ -41,10 +37,37 @@ router.patch("/edit", isAuthenticated, async (req, res, next) => {
   }
 });
 
-// PATH "api/profile/editPassword/:id" => Actualiza la contraseña del usuario
-router.patch("/editPassword")
+// PATH "api/profile/editPassword" => Actualiza la contraseña del usuario
+router.patch("/editPassword", isAuthenticated, async (req, res, next) => {
+  const {password} = req.body
 
-// DELETE "/api/profile/delete/:id" => Elimina un perfil de usuario
+  if (!password) {
+    res.status(400).json({ errorMessage: "Los campos deben estar llenos" });
+    return;
+  }
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({
+      errorMessage:
+        "La contraseña no es lo sufucientemente segura. Debe tener mínimo 8 caracteres, una mayúscula, una minúscula y un caracter especial",
+    });
+    return;
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    await User.findByIdAndUpdate(req.payload._id, {
+      password: hashPassword
+    })
+    res.json("Contraseña actualizada");
+  } catch (error) {
+    next(error)
+  }
+})
+
+// DELETE "/api/profile/delete" => Elimina un perfil de usuario
 
 router.delete("/delete", isAuthenticated, async (req, res, next) => {
   try {
